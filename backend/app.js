f51516be8779167cd6efd5d4a0cbb1c818c23720
siddhiii01 <siddhiigupta15@gmail.com>
2025-11-16 13:25:05 +0530

    Created a collection User

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import {auth} from "express-oauth2-jwt-bearer"
+import {auth} from "express-oauth2-jwt-bearer";
+import User from "./models/User.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+
+
 //Connecting to frontend
 //Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Your React app URL
+  origin: 'http://localhost:5173', // React Dev Server
   credentials: true
 }));
 
 //JWT using Auth0
 const jwtCheck = auth({
     audience: process.env.AUTH0_AUDIENCE,
-    issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}/`,
+    issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
     tokenSigningAlg: 'RS256'
 });
 
 
+
 // Apply only to protected routes, not global
-app.get('/authorized', jwtCheck, (req, res) => {
-    res.send("Secured Resource");
-});
+
 
 app.listen(port, () => {
     console.log(`Server is connected to port: ${port}`);
---
+++ backend/models/User.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+    auth0Id: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    email: String,
+    name: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
